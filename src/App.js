0d674b0547b51e1205e7395d1f3134afcc59ab44
Gadisha Zerari <gadisha@bitPesa.co>
2019-08-21 16:26:33 +0100

    Use Person component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React App</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Does this work now?"));
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working</p>
+        <Person />
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Does this work now?"));
   }
 }
 
