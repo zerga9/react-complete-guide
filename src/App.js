799c7c2947443695c9088db88da28e3018b06c85
Gadisha Zerari <gadisha@bitPesa.co>
2019-08-28 14:30:11 +0100

    Use function call to pass argument

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <button onClick={this.switchNameHandler.bind(this, "Gadiza")}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler("Gadiza")}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
