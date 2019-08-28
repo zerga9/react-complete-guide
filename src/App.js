3a009489859f78517a20714f9a48d497402bfa3a
Gadisha Zerari <gadisha@bitPesa.co>
2019-08-28 14:38:56 +0100

    Dynamically update name through input

--- src/App.js
+++ src/App.js
@@ -24,6 +24,16 @@ class App extends Component {
     })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 33 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 50 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -36,7 +46,8 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, "Gadi!!!")}>My hobbies: Racing</Person>
+          click={this.switchNameHandler.bind(this, "Gadi!!!")}
+          changed={this.nameChangedHandler}>My hobbies: Racing</Person>
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age} />
