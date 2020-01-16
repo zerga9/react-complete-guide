624a7a4622bc10e3445e4ea19c261907acddbc73
Gadisha Zerari <gadisha@bitPesa.co>
2020-01-16 14:42:56 +0000

    Styled Components and Dynamic Styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Person from './Person/Person';
 import styled from 'styled-components';
 
 const StyledButton = styled.button`
-  background-color: green;
+  background-color: ${props => props.alt ? 'red' : 'green'};
   color: white;
   font: inherit;
   border: 1px solid blue;
@@ -12,7 +12,7 @@ const StyledButton = styled.button`
   cursor: pointer;
 
   &:hover {
-    background-color: lightgreen;
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
     color: black;
   }
 `;
@@ -105,7 +105,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working</p>
-        <StyledButton onClick={this.togglePersonsHandler}>
+        <StyledButton alt = { this.state.showPersons } onClick={this.togglePersonsHandler}>
             Toggle Persons
           </StyledButton>
           {persons}
